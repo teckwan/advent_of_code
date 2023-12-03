1aa9c02feaa038cde1e0ee881e17d6992059423f
Teck Wan Wong <teckwan.wong@doctolib.com>
2023-12-03 03:22:05 +0100

    day 2 part 2

--- rust/aoc_2023/src/day_02/mod.rs
+++ rust/aoc_2023/src/day_02/mod.rs
@@ -34,8 +34,41 @@ fn part_1(games: &str, max_red: u8, max_green: u8, max_blue: u8) -> u32 {
         .sum()
 }
 
+fn part_2(games: &str) -> u32 {
+    games
+        .lines()
+        .map(|game| {
+            let grabs: Vec<&str> =
+                game.split(": ").last().unwrap().split("; ").collect();
+
+            let mut max_red = 0;
+            let mut max_green = 0;
+            let mut max_blue = 0;
+
+            for grab in grabs {
+                for colors in grab.split(", ") {
+                    let mut a = colors.split(" ");
+
+                    let count: u32 = a.next().unwrap().parse().unwrap();
+                    let color: &str = a.next().unwrap();
+
+                    match color {
+                        "red" => if count > max_red { max_red = count }
+                        "green" => if count > max_green { max_green = count },
+                        "blue" => if count > max_blue { max_blue = count },
+                        _ => {},
+                    }
+                }
+            }
+
+            max_red * max_green * max_blue
+        })
+        .sum()
+}
+
 pub fn run() {
     let games = include_str!("input.txt");
 
     println!("day_02_1 - {}", part_1(games, 12, 13, 14));
+    println!("day_02_2 - {}", part_2(games));
 }
