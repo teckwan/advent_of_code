4b41a7705b1ff3f1db1dbef5c052468e9aac9103
Teck Wan Wong <teckwan.wong@doctolib.com>
2023-12-04 23:43:08 +0100

    fix formatting

--- rust/aoc_2023/src/day_03/mod.rs
+++ rust/aoc_2023/src/day_03/mod.rs
@@ -11,7 +11,7 @@ fn neighbor_symbol(x: usize, y: usize, map: &Vec<Vec<char>>) -> bool {
             let c = line[i];
 
             if !c.is_numeric() && c != '.' {
-                return true
+                return true;
             }
         }
     }
@@ -35,20 +35,28 @@ fn part_1(input: &str) -> u32 {
                 number = number * 10 + map[y][x].to_digit(10).unwrap();
                 adj_symbol = adj_symbol || neighbor_symbol(x, y, &map);
             } else if number != 0 {
-                if adj_symbol { total += number }
+                if adj_symbol {
+                    total += number
+                }
 
                 number = 0;
                 adj_symbol = false;
             }
         }
 
-        if adj_symbol { total += number }
+        if adj_symbol {
+            total += number
+        }
     }
 
     total
 }
 
-fn find_gear(x: usize, y: usize, map: &Vec<Vec<char>>) -> Option<(usize, usize)> {
+fn find_gear(
+    x: usize,
+    y: usize,
+    map: &Vec<Vec<char>>,
+) -> Option<(usize, usize)> {
     let min_y = if y == 0 { y } else { y - 1 };
     let max_y = if y == map.len() - 1 { y } else { y + 1 };
 
@@ -59,7 +67,7 @@ fn find_gear(x: usize, y: usize, map: &Vec<Vec<char>>) -> Option<(usize, usize)>
 
         for i in min_x..=max_x {
             if line[i] == '*' {
-                return Some((j, i))
+                return Some((j, i));
             }
         }
     }
@@ -70,7 +78,8 @@ fn find_gear(x: usize, y: usize, map: &Vec<Vec<char>>) -> Option<(usize, usize)>
 fn part_2(input: &str) -> u32 {
     let map =
         Vec::from_iter(input.lines().map(|line| Vec::from_iter(line.chars())));
-    let mut gear_map = Vec::from_iter(map.iter().map(|line| vec![Vec::new(); line.len()]));
+    let mut gear_map =
+        Vec::from_iter(map.iter().map(|line| vec![Vec::new(); line.len()]));
 
     for y in 0..map.len() {
         let axis = &map[y];
@@ -83,8 +92,10 @@ fn part_2(input: &str) -> u32 {
 
                 match find_gear(x, y, &map) {
                     Some(c) => {
-                        if !gears.contains(&c) { gears.push(c) };
-                    },
+                        if !gears.contains(&c) {
+                            gears.push(c)
+                        };
+                    }
                     None => {}
                 }
             } else if number != 0 {
@@ -104,16 +115,21 @@ fn part_2(input: &str) -> u32 {
         }
     }
 
-    gear_map.iter().map(|line| {
-        line.iter().map(|v| {
-            if v.len() == 2 {
-                //println!("{} {}", v[0], v[1]);
-                v.iter().product()
-            } else {
-                0
-            }
-        }).sum::<u32>()
-    }).sum()
+    gear_map
+        .iter()
+        .map(|line| {
+            line.iter()
+                .map(|v| {
+                    if v.len() == 2 {
+                        //println!("{} {}", v[0], v[1]);
+                        v.iter().product()
+                    } else {
+                        0
+                    }
+                })
+                .sum::<u32>()
+        })
+        .sum()
 }
 
 pub fn run() {
